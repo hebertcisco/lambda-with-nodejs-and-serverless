a31c79af715e87ca73eacf8d0d865ac887a98e21
hebertcisco <hebertcisco@outlook.com>
2023-01-15 00:00:32 -0300

    first commit

---
+++ handler.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+module.exports.helloWorld = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hello World'
+      },
+      null,
+      2
+    ),
+  };
+};
+
